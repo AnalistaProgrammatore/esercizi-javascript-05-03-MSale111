7837cfff8b936cd4e0cc78eb1fc2cf49c71757c8
MSale111 <59830912+MSale111@users.noreply.github.com>
2020-03-15 12:26:56 +0100

    Array dei voti

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,17 @@
+let votiStudente = {
+    voti: [],
+    add(voto) {
+        this.voti.push(voto)
+    },
+    media () {
+        return this.voti.reduce((accumulatore, corrente) => accumulatore + corrente)/this.voti.length
+    }
+}
 
+votiStudente.add(7);
+votiStudente.add(8);
+votiStudente.add(4);
+votiStudente.add(6);
+
+console.log (votiStudente.voti);
+console.log (votiStudente.media());
